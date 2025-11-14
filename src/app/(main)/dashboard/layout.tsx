10eceecabd7e56181936ccf175d23a373089c4d6
Omar Mostafa <omar.mostafa0205@gmail.com>
2025-11-14 14:42:16 +0200

    fixed

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import  getServerSession  from "next-auth";
 import { authConfig } from "@/server/auth/config";
-import DashboardNav from "@/components/DashboardNav"
+import DashboardNav from "@/components/dashboard/DashboardNav"
 import { redirect } from "next/navigation";
 
 export default async function dashboardLayout ({ children }: { children: React.ReactNode }){
